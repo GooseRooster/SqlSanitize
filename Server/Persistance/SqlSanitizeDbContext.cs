0fe5114f2159ead321ff0b14e7d18dbc54913edb
Cameron Mostert <cameronmostert2099@gmail.com>
2023-06-05 21:12:45 +0200

    EFCore instantiation, register db context, seed initial data

--- Server/Persistance/SqlSanitizeDbContext.cs
+++ Server/Persistance/SqlSanitizeDbContext.cs
@@ -1,10 +1,91 @@
-﻿using System.Data.Entity;
+﻿using SqlSanitize.Shared;
+using Newtonsoft.Json;
+using Microsoft.EntityFrameworkCore;
 
 namespace SqlSanitize.Server.Persistance
 {
     public class SqlSanitizeDbContext : DbContext
     {
 
+        ILogger _logger;
+
+
+        public SqlSanitizeDbContext(ILogger<SqlSanitizeDbContext> logger, DbContextOptions<SqlSanitizeDbContext> options)
+        : base(options)
+        {
+            _logger = logger;
+        }
+
+
+
+        public DbSet<SensitiveMessage> SensitiveMessages { get; set; }
+        public DbSet<SqlSensitive> SqlSensitives { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLExpress;Database=SqlSanitize;Trusted_Connection=True;Encrypt=False");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SqlSensitive>()
+                .HasKey(x => x.Id);
+  
+
+            modelBuilder.Entity<SqlSensitive>().Property(x => x.Filter).IsRequired();
+
+            modelBuilder.Entity<SensitiveMessage>().HasKey(x => x.Id);
+            modelBuilder.Entity<SensitiveMessage>().Property(x => x.Message).IsRequired();
+
+
+            string sensitiveText = string.Empty;
+            List<string> sensitiveWords = new();
+            try
+            {
+                //load up the list of sensitive phrases for seeding
+                sensitiveText = File.ReadAllText("sql_sensitive_list.txt");
+
+                //parse as json string and load into a list of strings
+                sensitiveWords = JsonConvert.DeserializeObject<List<string>>(sensitiveText);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Fatal error reading data for SqlSensitive table!");
+         
+            }
+
+
+            if (string.IsNullOrEmpty(sensitiveText) || sensitiveWords == null || !sensitiveWords.Any())
+            {
+                _logger.LogError("Could not find data to load for SqlSensitive table!");
+                return;
+
+            }
+
+            //create a list of SqlSensitive objects
+            List<SqlSensitive> filtersToSave = new();
+            foreach (var word in sensitiveWords)
+            {
+                //if somehow the word or phrase is null or empty, skip it
+                if (string.IsNullOrEmpty(word))
+                    continue;
+
+                //instantiate a SqlSensitive object with Id
+                SqlSensitive toAdd = new();
+                toAdd.Filter = word;
+
+                filtersToSave.Add(toAdd);
+                
+
+            }
+
+            //populate the SqlSensitive table
+            modelBuilder.Entity<SqlSensitive>().HasData(filtersToSave);
+
+
+
+        }
 
 
 
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,12 +1,20 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using SqlSanitize.Server.Persistance;
+using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<SqlSanitizeDbContext>();
 builder.Services.AddRazorPages();
 builder.Services.AddSwaggerGen();
+
+
+
+
 
 var app = builder.Build();
 
@@ -21,6 +29,14 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<SqlSanitizeDbContext>();
+    // use context
+    dbContext.Database.EnsureCreated();
+}
+
 
 app.UseHttpsRedirection();
 
